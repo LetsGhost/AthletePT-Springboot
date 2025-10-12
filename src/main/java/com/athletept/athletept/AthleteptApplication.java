b4afabb29aa13aa6a992d30e716ed85a9c8cf085
Paul <ghostlest330@gmail.com>
2025-10-12 12:15:16 +0200

    init

---
+++ src/main/java/com/athletept/athletept/AthleteptApplication.java
@@ -0,0 +1,13 @@
+package com.athletept.athletept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteptApplication.class, args);
+	}
+
+}
